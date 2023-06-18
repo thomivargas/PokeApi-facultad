b7f70f5371cbf1cf3006c6828216022abcd5d1bc
DanielCastillaa <danielcastillaa123@gmail.com>
2023-06-17 21:22:57 -0300

    Router y mostrar los pokemones en el html

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit  } from '@angular/core';
 import { PokeapiService } from '../services/pokeapi.service';
 import { PokemonesTipo, Pokemones } from '../interfaces/interfaces'
+import { Tab2Page } from '../tab2/tab2.page';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,12 +11,12 @@ import { PokemonesTipo, Pokemones } from '../interfaces/interfaces'
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit{
-
+  component= Tab2Page;
   pokemonesTipo: PokemonesTipo[] = []
   pokemones: Pokemones[] = []
   pokemonNombres: string[] = [];
   pokemonId: string[] = []
-  constructor( private pokemonService: PokeapiService) {}
+  constructor( private pokemonService: PokeapiService, private router:Router) {}
 
   ngOnInit() {
     this.pokemonService.getTipoPokemones().subscribe((data: any) => {
@@ -23,11 +25,12 @@ export class Tab1Page implements OnInit{
   }
 
   async verPokemones(name: string){
-    this.pokemonService.getPokemones(name).subscribe(async (respuestaApi: any) => {
-      this.pokemones=respuestaApi.pokemon.slice(0,5)
-      this.pokemonNombres = respuestaApi.pokemon.slice(0, 5).map((pokemon: any) => pokemon.pokemon.name);
-      console.log(respuestaApi)
-    })
+    // this.pokemonService.getPokemones(name).subscribe(async (respuestaApi: any) => {
+    //   this.pokemones=respuestaApi.pokemon.slice(0,5)
+    //   this.pokemonNombres = respuestaApi.pokemon.slice(0, 5).map((pokemon: any) => pokemon.pokemon.name);
+    //   console.log(respuestaApi)
+    // })
+    this.router.navigate(['detalle-pokemones', name])
   };
   
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {PokeapiService} from '../services/pokeapi.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  data:any
+  pokemones: any;
+  urlImagen: any;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private PokemonService: PokeapiService) {
+    
+  }
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      console.log(id)
+      if (id) {
+        this.PokemonService.getPokemones(id).subscribe(respuesta => {
+          this.data = respuesta;
+          this.pokemones=this.data.pokemon.slice(0,5)
+          console.log(this.pokemones)
+          
+        });
+        this.PokemonService.getImagen(id).subscribe(respuesta =>{
+          console.log(respuesta)
+        })
+      }
+    });
+  }
+  async InfoPokemones(name:string){
+    this.PokemonService.getImagen(name).subscribe((respuesta:any) =>{
+      this.urlImagen=respuesta.sprites.front_default;
+    })
+  }
+
+
 
 }
